f6fe19c84f1e5aa8fa35ca63696260d58b267398
Jaysen <jaysendaemon@gmail.com>
2025-09-25 11:56:21 +0200

    Opdracht 2\

--- Assets/Parabola.cs
+++ Assets/Parabola.cs
@@ -3,14 +3,21 @@ using UnityEngine;
 public class Parabola : MonoBehaviour
 {
     [SerializeField] Point point;
-    private int numberOfPoints = 10;
+    private int numberOfPoints = 11;
+    Vector2 minScreen, maxScreen;   
     
     void Start()
     {
+        minScreen = Camera.main.ScreenToWorldPoint(Vector2.zero);   
+        maxScreen = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));   
+        
+        float dx = (maxScreen.x - minScreen.x) / numberOfPoints;
+        
         for (int i = 0; i < numberOfPoints; i++)
-        {
+        { 
+            float x_pos = minScreen.x + i * dx; 
            Point copyOfPoint = Instantiate(point);
-           copyOfPoint.transform.position = new Vector3(i, 0, 0);
+           copyOfPoint.transform.position = new Vector3(x_pos, 0, 0);
         }
     }
 
